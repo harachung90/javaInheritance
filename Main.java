769640c43b2bd6410fcfe2a27c56176c33a47393
Hara <harachung90@gmail.com>
2023-06-02 20:39:07 +1000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.makeSound();
+    }
+}
+
+class Animal {
+    String sound = "";
+
+    void makeSound() {
+        System.out.println(sound);
+    }
+}
+
+class Dog extends Animal {
+    Dog() {
+        sound = "bark";
+    }
+}
+
+
